84e2f821127b1a5f9d8c58b3b18ed62d2c31f5b9
Roman <romazaniosdev@gmail.com>
2026-04-21 12:02:12 +0300

    Initial Commit

---
+++ 145TilxanaVetrancrex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  145TilxanaVetrancrex
+//
+//  Created by Roman on 4/21/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
